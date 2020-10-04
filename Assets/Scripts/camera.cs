281e17f58ad5ba130e3d0c51681516ff787503b4
Stefan <stevetv90@gmail.com>
2020-10-04 22:46:02 +0200

    Day 2
    TODO: Ending

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    Quaternion rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerRot = player.transform.eulerAngles;
+        transform.position = new Vector3(playerPos.x,playerPos.y,transform.position.z);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,playerRot.z);
+        */ 
+        transform.position = new Vector3(transform.position.x,transform.position.y,-10); 
+    }
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+    void LateUpdate()
+    {
+        //transform.rotation = Quaternion.Euler(transform.rotation.x,transform.rotation.y,rotation.z);
+        //transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, 0);
+        transform.position = new Vector3(transform.position.x,transform.position.y,-10); 
+    } 
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    private bool onGround = true;
     private int state = 0;
     /*
     0= down
@@ -11,24 +12,67 @@ public class player : MonoBehaviour
     2= right
     3= up
     */
+    private float rotation = 0f;
     private float speed = 3f;
     private int mapsizeX = 30;
     private int mapsizeY = 20;
     private float maxVelocity = 10f;
 
-    // Update is called once per frame
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-        print(state);
-        if( state == 0 || state == 3)
+        float input = Input.GetAxisRaw("Horizontal")*speed; 
+        if( state == 0)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed, GetComponent<Rigidbody2D>().velocity.y); 
+            GetComponent<Rigidbody2D>().velocity = new Vector2(input, GetComponent<Rigidbody2D>().velocity.y); 
         }
-        else
+        else if(state == 1)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Input.GetAxisRaw("Vertical")*speed);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, input);
         }
-        
+        else if(state == 2)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -input);
+        }
+        else if(state == 3)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-input, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if(input != 0f)
+        {
+            
+            if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "running" && input > 0)
+            {
+                anim.Play("Base Layer.running", 0, 1);
+            }
+            else if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "running2" && input < 0)
+            {
+                anim.Play("Base Layer.running2", 0, 1);
+            }
+        }   
+        else if(Physics2D.Raycast(transform.position, -Vector2.up).collider != null)
+        {
+            if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "landing" && onGround == false)
+            {
+                //anim.Play("Base Layer.landing", 0, 1);
+                onGround = true;
+            }
+            else if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "Idel" && anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "landing")
+            {
+                anim.Play("Base Layer.Idel", 0, 1);
+            }
+        }
+        else if(Physics2D.Raycast(transform.position, -Vector2.up).collider == null)
+        {
+            onGround = false;
+        }
+
         if(GetComponent<Rigidbody2D>().velocity.y<-maxVelocity)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -maxVelocity); 
@@ -44,6 +88,24 @@ public class player : MonoBehaviour
         else if(GetComponent<Rigidbody2D>().velocity.x>maxVelocity)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(maxVelocity, GetComponent<Rigidbody2D>().velocity.y); 
+        }
+
+        if(transform.rotation.eulerAngles.z != rotation)
+        {
+            GetComponent<Rigidbody2D>().freezeRotation = false;
+            if(transform.rotation.eulerAngles.z < rotation+1 && transform.rotation.eulerAngles.z > rotation-1)
+            {
+                transform.rotation = Quaternion.Euler(transform.rotation.x,transform.rotation.y,rotation);
+            }
+            else if (transform.rotation.eulerAngles.z < rotation)
+            {
+                transform.Rotate(new Vector3(0,0,1f));
+            }
+            else if(transform.rotation.eulerAngles.z > rotation)
+            {
+                transform.Rotate(new Vector3(0,0,-1f));
+            }
+            GetComponent<Rigidbody2D>().freezeRotation = true;
         }
 
         if(transform.position.x < -mapsizeX)
@@ -65,38 +127,38 @@ public class player : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.name == "Left" && state == 0)
-        {
-            print("LEFT");
-            state += 1;
-            Physics2D.gravity = new Vector3(-9.81f,0,0);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); 
-        }
-        else if(col.gameObject.name == "Right" && state == 1)
+        if(col.gameObject.name == "Right")
         {
             print("RIGHT");
-            state += 1;
+            state = 1;
             Physics2D.gravity = new Vector3(9.81f,0,0);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); 
+            rotation = 90f;
         }
-        else if(col.gameObject.name == "Up" && state == 2)
+        else if(col.gameObject.name == "Left")
+        {
+            print("LEFT");
+            state = 2;
+            Physics2D.gravity = new Vector3(-9.81f,0,0);
+            rotation = 270f;
+        }
+        else if(col.gameObject.name == "Up")
         {
             print("UP");
-            state += 1;
+            state = 3;
             Physics2D.gravity = new Vector3(0,9.81f,0);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); 
+            rotation = 180f;
         }
-        else if(col.gameObject.name == "Down" && state == 3)
+        else if(col.gameObject.name == "Down")
         {
             print("DOWN");
             state = 0;
             Physics2D.gravity = new Vector3(0,-9.81f,0);
-            GetComponent<Rigidbody>().velocity = new Vector2(0, 0); //Funkar inte
+            rotation = 0f;
         }
         else if(col.gameObject.name == "Exit" && state == 3)
         {
             //Win
-            gameObject.GetComponent<Renderer2D>().enabled = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
         }
     }
 }
